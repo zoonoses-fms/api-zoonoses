098316864aa9c8811dade85f6ca972da583ac1ca
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2022-08-08 10:55:22 -0300

    bug fix store geometry

--- app/Http/Controllers/Api/V1/Ncrlo/VaccinationSupportController.php
+++ app/Http/Controllers/Api/V1/Ncrlo/VaccinationSupportController.php
@@ -94,9 +94,11 @@ class VaccinationSupportController extends Controller
             $vaccinationSupport
             ->the_neighborhood_alias_id = $neighborhoodAlias->id;
         }
-        $geometry = json_encode($request->geometry);
+        if ($request->geometry != null) {
+            $geometry = json_encode($request->geometry);
 
-        $vaccinationSupport->geometry = DB::raw("ST_SetSRID(ST_GeomFromGeoJSON('{$geometry}'), 3857)");
+            $vaccinationSupport->geometry = DB::raw("ST_SetSRID(ST_GeomFromGeoJSON('{$geometry}'), 3857)");
+        }
 
         $vaccinationSupport->save();
         return $vaccinationSupport;
