7d6362cc1a015f6d89fdcb0ab9c250b1a79b6caf
Thiago Pinto Dias <thiagopinto.lx@gmail.com>
2022-09-01 13:41:24 -0300

    change width animate map

---
+++ database/migrations/2022_09_01_133019_alter_table_campaing_points_change_order.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('campaing_points', function (Blueprint $table) {
+            $table->float('order', 8, 2)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('campaing_points', function (Blueprint $table) {
+            //
+        });
+    }
+};
